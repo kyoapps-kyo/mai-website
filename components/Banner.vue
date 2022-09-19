e56a686629e8cf783aabfd6c2b0cf5931ceb4a68
kyoapps-kyo <kyo@kyoapps.com>
2022-09-19 13:35:59 +0900

    mobile banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,30 +1,35 @@
 <template>
   <div>
-    <div class="banner h-[720px] w-full">
-      <div class="h-[720px] lg:w-[1200px] mx-auto relative">
+    <div class="banner md:h-[720px] md:min-w-[1200px]">
+      <div class="h-[720px] w-full lg:w-[1200px] md:mx-auto relative">
         <div
-          class="absolute top-1/2 mt-[-174px] h-[348px] w-[984px] left-1/2 ml-[-492px] bg-opacity-[0.5] bg-black"
+          class="absolute top-1/2 md:mt-[-174px] mt-[-220px] h-[440px] md:h-[348px] md:w-[984px] w-full md:left-1/2 md:ml-[-492px] bg-opacity-[0.5] bg-black"
         >
           <img
-            class="absolute right-[86px] w-[200px] h-[301px] top-1/2 mt-[-150px]"
+            class="absolute md:right-[86px] md:w-[200px] md:h-[301px] w-[100px] h-[150px] md:top-1/2 md:mt-[-150px] top-[20px] right-1/2 mr-[-50px]"
             src="~/assets/images/word.png"
             alt=""
           />
           <div
-            class="h-[260px] w-[594px] flex flex-col justify-between mt-[34px] ml-[78px]"
+            class="md:h-[260px] md:w-[594px] flex flex-col justify-end items-center md:items-stretch md:justify-between md:mt-[34px] md:ml-[78px] w-full h-full pb-10 md:pb-0"
           >
-            <p class="goldtext w-[432px] h-[70px] leading-[70px] text-[48px]">
+            <p
+              class="goldtext md:w-[432px] md:h-[70px] leading-[70px] text-[28px] md:text-[48px]"
+            >
               烏丸御池で新開店！
             </p>
             <p
-              class="goldtext w-[480px] h-[70px] leading-[70px] text-[48px] ml-[70px]"
+              class="goldtext md:w-[480px] md:h-[70px] leading-[70px] text-[28px] md:text-[48px] md:ml-[70px]"
             >
               高級リラクゼーション
             </p>
             <p
-              class="goldtext w-[480px] h-[70px] leading-[70px] text-[32px] ml-[70px]"
+              class="goldtext md:w-[480px] md:h-[70px] leading-[70px] text-[28px] md:ml-[70px]"
             >
-              予約電話<span class="bulintext">090-9846-8392</span>
+              <span class="hidden md:inline">予約電話</span
+              ><span class="bulintext md:ml-[30px] md:text-[42px] text-[34px]"
+                >090-9846-8392</span
+              >
             </p>
           </div>
         </div>
@@ -49,15 +54,12 @@
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   background-clip: text;
-  text-fill-color: transparent;
 
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 
 .bulintext {
   display: inline-block;
-  margin-left: 30px;
-  font-size: 42px;
   animation: bulin 1s linear infinite alternate;
 }
 
