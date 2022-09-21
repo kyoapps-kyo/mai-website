19323470db993f7239e1fe52554665341cc20b9f
kyoapps-kyo <kyo@kyoapps.com>
2022-09-21 09:07:25 +0900

    generate

--- components/Tel.vue
+++ components/Tel.vue
@@ -5,7 +5,7 @@
     </p>
     <p class="text-center text-[24px] text-[#ffd100]">090-9846-8392</p>
     <p class="text-center text-[12px] text-[#d6d6d6]">
-      受付時間：AM 12:00 - last <br />【予約優先】【非通知不可】
+      受付時間：PM12:00 - LAST <br />【予約優先】【非通知不可】
     </p>
   </div>
 </template>
--- dist/_nuxt/static/1663718609/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fjobs","\u002F","\u002Fmenu","\u002Faccess","\u002Fstaff"]})
