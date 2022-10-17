f8294aea76ddcebe40562cc36598a32f23b012ea
kyoapps-kyo <kyo@kyoapps.com>
2022-10-18 08:03:36 +0900

    map info update

--- components/Map.vue
+++ components/Map.vue
@@ -2,7 +2,7 @@
   <div class="mx-auto w-max">
     <iframe
       class=""
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d204.24561325274658!2d135.7613919354226!3d35.00846207008786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001088556159387%3A0x933c7a3bc25e554b!2z44CSNjA0LTgxMzEg5Lqs6YO95bqc5Lqs6YO95biC5Lit5Lqs5Yy66I-x5bGL55S677yT77yWIOODl-ODqeOCtuOCs-ODqeODoA!5e0!3m2!1sja!2sjp!4v1663513176413!5m2!1sja!2sjp"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1147.3965652109323!2d135.76107671523786!3d35.00919028949807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60010926e5b97bcb%3A0x69c80ccadc4482ab!2z6Iie!5e0!3m2!1sja!2sjp!4v1666046754614!5m2!1sja!2sjp"
       :width="width"
       :height="height"
       style="border: 0"
